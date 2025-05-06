92bf470870d0774a1bc29b5f053ed948a059a52e
Jakapat Pattanasiri <jakapat_pattanasiri@hotmail.com>
2025-05-06 14:20:34 +0700

    added cors

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.set("trust proxy", 1);
 // Global middlewares
 app.use(helmet());
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://rag-notes-frontend.vercel.app"], // your frontend domain
+  origin: ["http://localhost:5173", "https://mini-project-frontend-silk.vercel.app"], // your frontend domain
   credentials: true, // ✅ allow cookies to be sent
 };
 
